57ed3529ab5b851ca82e9626150ba1efa9636ed5
Sergiu <sergiuosvat@yahoo.com>
2023-03-29 00:23:56 +0300

    update code

--- a1/a1.c
+++ a1/a1.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <dirent.h>
 
 int list_dir(const char *path, const char *filter)
@@ -39,9 +40,9 @@ int list_dir(const char *path, const char *filter)
         }
         else if (strcmp(filter, "has_perm_write") == 0)
         {
-            if(lstat(fullPath, &statbuf) == 0)
+            if (lstat(fullPath, &statbuf) == 0)
             {
-                if(statbuf.st_uid == getuid() && (statbuf.st_mode & S_IWUSR) != 0)
+                if (statbuf.st_uid == getuid() && (statbuf.st_mode & S_IWUSR) != 0)
                 {
                     printf("%s\n", fullPath);
                 }
@@ -81,9 +82,9 @@ void list_dir_rec(const char *path, const char *filter)
         snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
         if (strcmp(filter, "has_perm_write") == 0)
         {
-            if(lstat(fullPath, &statbuf) == 0)
+            if (lstat(fullPath, &statbuf) == 0)
             {
-                if(statbuf.st_uid == getuid() && (statbuf.st_mode & S_IWUSR) != 0)
+                if (statbuf.st_uid == getuid() && (statbuf.st_mode & S_IWUSR) != 0)
                 {
                     printf("%s\n", fullPath);
                 }
@@ -105,31 +106,47 @@ void list_dir_rec(const char *path, const char *filter)
     closedir(dir);
 }
 
-int parse(const char* path)
+int parse(const char *path)
 {
-    FILE* fp;
-    fp = fopen(path,"r");
+    int fd, version, nr_sect, hs;
+    fd = open(path, O_RDONLY);
     char magic[3];
-    int hs;
-    if(fp == NULL)
+    if (fd == -1)
     {
         puts("ERORR");
         puts("Could not open file");
         return -1;
     }
-    fseek(fp,-1,SEEK_END);
-    magic[0] = fgetc(fp);
-    fseek(fp,-2,SEEK_END);
-    magic[1] = fgetc(fp);
-    if(magic[0] != 'I' || magic[1] != 'Z')
+    lseek(fd, -1, SEEK_END);
+    read(fd, &magic[0], 1);
+    lseek(fd, -2, SEEK_END);
+    read(fd, &magic[1], 1);
+    if (magic[0] != 'I' || magic[1] != 'Z')
     {
         puts("ERROR");
         puts("wrong magic");
+        return -1;
     }
-    fseek(fp,-3,SEEK_END);
-    fscanf(fp,"%d",&hs);
-    
-    fclose(fp);
+    lseek(fd, -4, SEEK_END);
+    read(fd, &hs, 2);
+    lseek(fd, -hs, SEEK_END);
+    read(fd, &version, 1);
+    if (version < 23 || version > 65)
+    {
+        puts("ERROR");
+        puts("wrong version");
+        return -1;
+    }
+    read(fd, &nr_sect, 1);
+    if (nr_sect < 6 || nr_sect > 14)
+    {
+        puts("ERROR");
+        puts("wrong sect_nr");
+        return -1;
+    }
+    printf("SUCCESS\n");
+
+    close(fd);
     return 0;
 }
 
@@ -175,9 +192,9 @@ int main(int argc, char **argv)
                 list_dir_rec(path, filter);
             }
         }
-        if(strcmp(argv[1], "parse") == 0)
+        if (strcmp(argv[1], "parse") == 0)
         {
-            strcpy(path, argv[2]+5);
+            strcpy(path, argv[2] + 5);
             parse(path);
         }
         free(path);
