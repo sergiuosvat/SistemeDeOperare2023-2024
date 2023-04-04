4c8c48fc84502def611969757ad4fd142ca645d3
Sergiu <sergiuosvat@yahoo.com>
2023-04-05 01:10:36 +0300

    update code

--- a1/a1.c
+++ a1/a1.c
@@ -61,8 +61,6 @@ int list_dir(const char *path, const char *filter)
         {
             printf("%s\n", fullPath);
         }
-
-        memset(fullPath, 0, sizeof(fullPath));
     }
     closedir(dir);
     return 0;
@@ -110,14 +108,14 @@ void list_dir_rec(const char *path, const char *filter)
                 list_dir_rec(fullPath, filter);
             }
         }
-        memset(fullPath, 0, sizeof(fullPath));
     }
     closedir(dir);
 }
 
 int parse(const char *path)
 {
-    int fd, version = 0, nr_sect = 0, hs;
+    int fd,version, nr_sect = 0;
+    short hs;
     fd = open(path, O_RDONLY);
     char magic[3];
     if (fd == -1)
@@ -153,7 +151,7 @@ int parse(const char *path)
         puts("wrong sect_nr");
         return -1;
     }
-    sec_header* arr = malloc(nr_sect*sizeof(sec_header));
+    sec_header* arr = calloc(nr_sect,sizeof(sec_header));
     for (int i = 0; i < nr_sect; i++)
     {
         read(fd, arr[i].name, 15);
@@ -179,11 +177,75 @@ int parse(const char *path)
     return 0;
 }
 
+int extract(const char* path, int nr, int line)
+{
+    int fd1, version1 = 0, nr_sect1 = 0, hs1 = 0;
+    int nr_lines = 1;
+    off_t off;
+    fd1 = open(path, O_RDONLY);
+    if(fd1 == -1)
+    {
+        puts("ERROR");
+        puts("invalid file");
+    }
+    lseek(fd1, -4, SEEK_END);
+    read(fd1, &hs1, 2);
+    lseek(fd1, -hs1, SEEK_END);
+    read(fd1, &version1, 1);
+    read(fd1, &nr_sect1, 1);
+    if(nr>nr_sect1)
+    {
+        puts("ERROR");
+        puts("invalid section");
+    }
+    sec_header* array = calloc(nr_sect1+1,sizeof(sec_header));
+    for (int i = 0; i < nr_sect1; i++)
+    {
+        read(fd1, array[i+1].name, 15);
+        read(fd1, &array[i+1].type, 2);
+        read(fd1,&array[i+1].offset, 4);
+        read(fd1,&array[i+1].size,4);
+    }
+    off = lseek(fd1,array[nr].offset+array[nr].size,SEEK_SET);
+    lseek(fd1,off,SEEK_SET);
+    char* buffer = calloc(1,sizeof(char));
+    while (line != nr_lines)
+    {
+        read(fd1,buffer,1);
+        lseek(fd1,-2,SEEK_CUR);
+        if(*buffer == '\n')
+        {
+            nr_lines++;
+        }
+    }
+    buffer[0] = 0;
+    if(nr_lines != line)
+    {
+        puts("ERROR");
+        puts("invalid line");
+        return -1;
+    }
+    puts("SUCCESS");
+    lseek(fd1,1,SEEK_CUR);
+    while (lseek(fd1,-2,SEEK_CUR) > (array[nr].offset-1) && buffer[0] != '\n') 
+    {
+        read(fd1, buffer, 1);
+        printf("%c", buffer[0]);
+    }
+    free(array);
+    free(buffer);
+    close(fd1);
+    return 0;
+}
+
+
 int main(int argc, char **argv)
 {
     char *recursive = NULL;
     char *filter = (char *)calloc(512, sizeof(char));
     char *path = (char *)calloc(512, sizeof(char));
+    char* sect = (char*) calloc(512,sizeof(char));
+    char* line = (char*) calloc(512,sizeof(char));
     if (argc >= 2)
     {
         if (strcmp(argv[1], "variant") == 0)
@@ -226,8 +288,17 @@ int main(int argc, char **argv)
             strcpy(path, argv[2] + 5);
             parse(path);
         }
+        if (strcmp(argv[1], "extract") == 0)
+        {
+            strcpy(path, argv[2] + 5);
+            strcpy(sect,argv[3]+8);
+            strcpy(line,argv[4] + 5);
+            extract(path,atoi(sect), atoi(line));
+        }
         free(path);
         free(filter);
+        free(sect);
+        free(line);
     }
 
     return 0;
