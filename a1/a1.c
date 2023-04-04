d23bebaa180b003c9e5816a6f647b90f239a0267
Sergiu <sergiuosvat@yahoo.com>
2023-04-05 02:08:28 +0300

    almost done

--- a1/a1.c
+++ a1/a1.c
@@ -15,7 +15,6 @@ typedef struct sec_header
     int size;
 } sec_header;
 
-
 int list_dir(const char *path, const char *filter)
 {
     DIR *dir = NULL;
@@ -29,12 +28,6 @@ int list_dir(const char *path, const char *filter)
         return -1;
     }
     dir = opendir(path);
-    if (dir == NULL)
-    {
-        puts("ERORR");
-        puts("Could not open file");
-        return -1;
-    }
     printf("SUCCESS\n");
     while ((entry = readdir(dir)) != NULL)
     {
@@ -114,7 +107,7 @@ void list_dir_rec(const char *path, const char *filter)
 
 int parse(const char *path)
 {
-    int fd,version, nr_sect = 0;
+    int fd, version, nr_sect = 0;
     short hs;
     fd = open(path, O_RDONLY);
     char magic[3];
@@ -151,39 +144,40 @@ int parse(const char *path)
         puts("wrong sect_nr");
         return -1;
     }
-    sec_header* arr = calloc(nr_sect,sizeof(sec_header));
+    sec_header *arr = calloc(nr_sect, sizeof(sec_header));
     for (int i = 0; i < nr_sect; i++)
     {
         read(fd, arr[i].name, 15);
         read(fd, &arr[i].type, 2);
-        read(fd,&arr[i].offset, 4);
-        read(fd,&arr[i].size,4);
-        if (arr[i].type != 51 && arr[i].type!= 69 && arr[i].type != 63 && arr[i].type != 45 && arr[i].type != 53 && arr[i].type != 90 && arr[i].type != 40)
+        read(fd, &arr[i].offset, 4);
+        read(fd, &arr[i].size, 4);
+        if (arr[i].type != 51 && arr[i].type != 69 && arr[i].type != 63 && arr[i].type != 45 && arr[i].type != 53 && arr[i].type != 90 && arr[i].type != 40)
         {
             puts("ERROR");
             puts("wrong sect_types");
+            free(arr);
             return -1;
         }
     }
     printf("SUCCESS\n");
     printf("version=%d\n", version);
     printf("nr_sections=%d\n", nr_sect);
-    for(int i = 0;i<nr_sect;i++)
+    for (int i = 0; i < nr_sect; i++)
     {
-        printf("section%d: %s %d %d\n", i+1, arr[i].name, arr[i].type, arr[i].size);
+        printf("section%d: %s %d %d\n", i + 1, arr[i].name, arr[i].type, arr[i].size);
     }
     free(arr);
     close(fd);
     return 0;
 }
 
-int extract(const char* path, int nr, int line)
+int extract(const char *path, int nr, int line)
 {
     int fd1, version1 = 0, nr_sect1 = 0, hs1 = 0;
     int nr_lines = 1;
     off_t off;
     fd1 = open(path, O_RDONLY);
-    if(fd1 == -1)
+    if (fd1 == -1)
     {
         puts("ERROR");
         puts("invalid file");
@@ -193,41 +187,41 @@ int extract(const char* path, int nr, int line)
     lseek(fd1, -hs1, SEEK_END);
     read(fd1, &version1, 1);
     read(fd1, &nr_sect1, 1);
-    if(nr>nr_sect1)
+    if (nr > nr_sect1)
     {
         puts("ERROR");
         puts("invalid section");
     }
-    sec_header* array = calloc(nr_sect1+1,sizeof(sec_header));
+    sec_header *array = calloc(nr_sect1 + 1, sizeof(sec_header));
     for (int i = 0; i < nr_sect1; i++)
     {
-        read(fd1, array[i+1].name, 15);
-        read(fd1, &array[i+1].type, 2);
-        read(fd1,&array[i+1].offset, 4);
-        read(fd1,&array[i+1].size,4);
+        read(fd1, array[i + 1].name, 15);
+        read(fd1, &array[i + 1].type, 2);
+        read(fd1, &array[i + 1].offset, 4);
+        read(fd1, &array[i + 1].size, 4);
     }
-    off = lseek(fd1,array[nr].offset+array[nr].size,SEEK_SET);
-    lseek(fd1,off,SEEK_SET);
-    char* buffer = calloc(1,sizeof(char));
+    off = lseek(fd1, array[nr].offset + array[nr].size, SEEK_SET);
+    lseek(fd1, off, SEEK_SET);
+    char *buffer = calloc(1, sizeof(char));
     while (line != nr_lines)
     {
-        read(fd1,buffer,1);
-        lseek(fd1,-2,SEEK_CUR);
-        if(*buffer == '\n')
+        read(fd1, buffer, 1);
+        lseek(fd1, -2, SEEK_CUR);
+        if (*buffer == '\n')
         {
             nr_lines++;
         }
     }
     buffer[0] = 0;
-    if(nr_lines != line)
+    if (nr_lines != line)
     {
         puts("ERROR");
         puts("invalid line");
         return -1;
     }
     puts("SUCCESS");
-    lseek(fd1,1,SEEK_CUR);
-    while (lseek(fd1,-2,SEEK_CUR) > (array[nr].offset-1) && buffer[0] != '\n') 
+    lseek(fd1, 1, SEEK_CUR);
+    while (lseek(fd1, -2, SEEK_CUR) > (array[nr].offset - 1) && buffer[0] != '\n')
     {
         read(fd1, buffer, 1);
         printf("%c", buffer[0]);
@@ -238,14 +232,112 @@ int extract(const char* path, int nr, int line)
     return 0;
 }
 
+int findall(const char *path, int flag)
+{
+    if (path == NULL)
+    {
+        puts("ERROR");
+        puts("Invalid directory path");
+        return -1;
+    }
+    DIR *dir = NULL;
+    struct dirent *entry = NULL;
+    char fullPath[512] = {};
+    dir = opendir(path);
+    struct stat statbuf = {};
+    int fd1, nr_sect1 = 0, version1 = 0, nr_lines = 1;
+    short hs1;
+    off_t off;
+    if (flag)
+    {
+        puts("SUCCESS");
+        flag = 0;
+    }
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+        snprintf(fullPath, 512, "%s/%s", path, entry->d_name);
+        if (lstat(fullPath, &statbuf) == 0)
+        {
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                findall(fullPath, flag);
+            }
+            else
+            {
+                fd1 = open(fullPath, O_RDONLY);
+                char magic[3];
+                lseek(fd1, -1, SEEK_END);
+                read(fd1, &magic[0], 1);
+                lseek(fd1, -2, SEEK_END);
+                read(fd1, &magic[1], 1);
+                if (magic[0] != 'I' || magic[1] != 'Z')
+                {
+                    continue;
+                }
+                lseek(fd1, -4, SEEK_END);
+                read(fd1, &hs1, 2);
+                lseek(fd1, -hs1, SEEK_END);
+                read(fd1, &version1, 1);
+                if (version1 < 23 || version1 > 65)
+                {
+                    continue;
+                }
+                read(fd1, &nr_sect1, 1);
+                if (nr_sect1 < 6 || nr_sect1 > 14)
+                {
+                    continue;
+                }
+                sec_header *array = calloc(nr_sect1 + 1, sizeof(sec_header));
+                for (int i = 0; i < nr_sect1; i++)
+                {
+                    read(fd1, array[i + 1].name, 15);
+                    read(fd1, &array[i + 1].type, 2);
+                    read(fd1, &array[i + 1].offset, 4);
+                    read(fd1, &array[i + 1].size, 4);
+                }
+                for (int i = 0; i < nr_sect1; i++)
+                {
+                    off = lseek(fd1, array[i + 1].offset + array[i + 1].size, SEEK_SET);
+                    lseek(fd1, off, SEEK_SET);
+                    char *buffer = calloc(1, sizeof(char));
+                    while (off >= array[i + 1].offset)
+                    {
+                        read(fd1, buffer, 1);
+                        lseek(fd1, -2, SEEK_CUR);
+                        if (*buffer == '\n')
+                        {
+                            nr_lines++;
+                        }
+                        off--;
+                    }
+                    free(buffer);
+                    if (nr_lines > 13)
+                    {
+                        printf("%s\n", fullPath);
+                        break;
+                    }
+                }
+                free(array);
+                close(fd1);
+            }
+        }
+    }
+    closedir(dir);
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
     char *recursive = NULL;
     char *filter = (char *)calloc(512, sizeof(char));
     char *path = (char *)calloc(512, sizeof(char));
-    char* sect = (char*) calloc(512,sizeof(char));
-    char* line = (char*) calloc(512,sizeof(char));
+    char *sect = (char *)calloc(512, sizeof(char));
+    char *line = (char *)calloc(512, sizeof(char));
+    int flag = 1;
     if (argc >= 2)
     {
         if (strcmp(argv[1], "variant") == 0)
@@ -291,9 +383,14 @@ int main(int argc, char **argv)
         if (strcmp(argv[1], "extract") == 0)
         {
             strcpy(path, argv[2] + 5);
-            strcpy(sect,argv[3]+8);
-            strcpy(line,argv[4] + 5);
-            extract(path,atoi(sect), atoi(line));
+            strcpy(sect, argv[3] + 8);
+            strcpy(line, argv[4] + 5);
+            extract(path, atoi(sect), atoi(line));
+        }
+        if (strcmp(argv[1], "findall") == 0)
+        {
+            strcpy(path, argv[2] + 5);
+            findall(path, flag);
         }
         free(path);
         free(filter);
